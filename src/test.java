87294ff37dce57f568a5a59ae14e26b454d88cc1
979007117@qq.com <979007117@qq.com>
2021-01-06 22:34:23 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("git");
+        System.out.println("ok");
+        System.out.println("github");
+    }
+}
